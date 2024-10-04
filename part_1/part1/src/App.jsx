3e492f1237d3a858724f2689ce8981ce474df173
sashavrg <168757785+sashavrg@users.noreply.github.com>
2024-10-04 15:58:10 +0200

    4/10 updates

--- part_1/part1/src/App.jsx
+++ part_1/part1/src/App.jsx
@@ -1,18 +1,29 @@
-const Hello = () => {
+const Hello = (props) => {
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello {props.name}, you are {props.age} years old.</p>
+    </div>
+  )
+}
+
+const Greetings = (props) => {
+  return (
+    <div>
+      <h1>Greetings, {props.animal}</h1>
     </div>
   )
 }
 
 const App = () => {
+  const name = 'Shoto'
+  const age = 3
+
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello />
-      <Hello />
-      <Hello />
+      <Greetings animal= 'Cats'/>
+      <Hello name= {name} age= {age}/>
+      <Hello name='Namira' age= '9'/>
+      <Hello name='Spettro' age= '4'/>
     </div>
   )
 }
