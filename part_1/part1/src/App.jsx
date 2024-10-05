ec04cffc222fc13d9e039520d0985887b4950bee
sashavrg <168757785+sashavrg@users.noreply.github.com>
2024-10-05 14:52:17 +0200

    5/10 update

--- part_1/part1/src/App.jsx
+++ part_1/part1/src/App.jsx
@@ -1,16 +1,17 @@
 const Hello = (props) => {
+  console.log(props)
   return (
-    <div>
+    <>
       <p>Hello {props.name}, you are {props.age} years old.</p>
-    </div>
+    </>
   )
 }
 
 const Greetings = (props) => {
   return (
-    <div>
+    <>
       <h1>Greetings, {props.animal}</h1>
-    </div>
+    </>
   )
 }
 
@@ -19,12 +20,12 @@ const App = () => {
   const age = 3
 
   return (
-    <div>
+    <>
       <Greetings animal= 'Cats'/>
       <Hello name= {name} age= {age}/>
-      <Hello name='Namira' age= '9'/>
-      <Hello name='Spettro' age= '4'/>
-    </div>
+      <Hello name='Namira' age= {5+4}/>
+      <Hello name='Spettro' age= {4}/>
+    </>
   )
 }
 
