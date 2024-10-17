5c24c07668a1a61b10b82f1a17e08a249ed62593
sashavrg <168757785+sashavrg@users.noreply.github.com>
2024-10-17 14:49:06 +0200

    Event handlers update

--- part_1/page_re-rendering/src/App.jsx
+++ part_1/page_re-rendering/src/App.jsx
@@ -1,8 +1,81 @@
-const App = (props) => {
-  const {counter} = props
+import { useState } from 'react'
+
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
   return (
-    <div>{counter}</div>
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
   )
 }
+
+const App = () => {
+  const [clicks, setClicks] = useState({
+    left: 0, right: 0
+  })
+  var [allClicks, setAll] = useState([])
+
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+  setClicks({...clicks, left: clicks.left +1})
+  setAll(allClicks.concat('L'))
+  setTotal(total +1)
+  }
+
+  const handleRightClick = () => {
+  setClicks({...clicks, right: clicks.right +1})
+  setAll(allClicks.concat('R'))
+  setTotal(total +1)
+  }
+
+
+  const handleresetClick = () => {
+    setClicks({left: 0, right: 0})
+    setTotal(0)
+  }
+  const handleresetAllClicks = () => setAll([])
+  console.log(allClicks)
+
+
+  return (
+    <>
+      <Display counter={clicks.left}/>
+      <Display counter={clicks.right}/>
+      <Button
+        onClick={handleLeftClick}
+        text='left'
+      />
+      <Button
+        onClick={handleRightClick}
+        text='right'
+      />
+      <Button
+        onClick={handleresetClick}
+        text='reset'
+        />
+      <Button onClick={handleresetAllClicks}
+      text='reset list'
+      />
+      <History allClicks={allClicks}/>
+      <p>{total}</p>
+    </>
+  )
+}
+
+
 
 export default App
--- part_1/page_re-rendering/src/main.jsx
+++ part_1/page_re-rendering/src/main.jsx
@@ -2,12 +2,5 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-let counter = 1
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
-const refresh = () => {
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <App counter={counter} />
-  )
-}
-
-refresh()
