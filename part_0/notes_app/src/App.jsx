6c5b4c36508ac9a78aeaecf46bc0bed947d84e9c
sashavrg <168757785+sashavrg@users.noreply.github.com>
2025-02-25 14:57:03 +0100

    JSON server
    created reference to JSON server for development

--- part_0/notes_app/src/App.jsx
+++ part_0/notes_app/src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+    axios.get('http://localhost:3001/notes').then((response) => {
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    })
+  }, [])
+  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
   event.preventDefault()
@@ -20,8 +30,12 @@ const App = (props) => {
   }
 
   const handleNoteChange = event => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewNote(event.target.value)
+  }
+
+  const handleShowButton = event => {
+    setShowAll(!showAll)
   }
 
   const notesToShow = showAll
@@ -31,6 +45,11 @@ const App = (props) => {
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={handleShowButton}>
+          show {showAll ? ` important` : ` all`}
+        </button>
+      </div>
       <ul>
         {notesToShow.map(note =>
         <Note key={note.id} note={note} />
